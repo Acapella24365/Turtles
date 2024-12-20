a19f573b021b0d07e9ff7872af06b846fa9b8246
Acapella24365 <syedahayaamzahra@gmail.com>
2024-12-20 15:47:48 +0500

    Turtle

---
+++ Turtle.py
@@ -0,0 +1,9 @@
+import turtle
+
+turtle.Screen().bgcolor("white")
+
+sc = turtle.Screen()
+sc.setup(250,250)
+
+turtle.title("Welcome to the Turtle Window")
+turtle.done()
---
+++ tu.py
@@ -0,0 +1,33 @@
+import turtle
+
+turtle.Screen().bgcolor("Grey")
+
+board = turtle.Turtle()
+
+#Triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+
+#Square
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+
+#Rectangle
+board.forward(200)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(200)
+board.left(90)
+board.forward(100)
+board.left(90)
